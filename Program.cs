471249c8f9c22aa69c75e2218851d5b0f20cdaf4
Artem <krivenkoff2013@yandex.ru>
2023-10-29 13:30:13 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N <= 1)
+        {
+            Console.WriteLine("Натуральные числа в промежутке от N до 1: ");
+        }
+        else
+        {
+            Console.WriteLine("Натуральные числа в промежутке от N до 1: ");
+            PrintNaturalNumbers(N);
+        }
+    }
+
+    static void PrintNaturalNumbers(int N)
+    {
+        if (N >= 1)
+        {
+            Console.Write(N + " ");
+            PrintNaturalNumbers(N - 1);
+        }
+    }
+}
